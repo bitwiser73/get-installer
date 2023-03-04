e019a32e7511321f90b70ba6a02be85bc20cf9f7
bitwiser73 <bitwiser73@users.noreply.github.com>
2023-03-04 12:41:53 +0100

    Add 'Inkscape'

--- Get-Installer.ps1
+++ Get-Installer.ps1
@@ -254,8 +254,84 @@ function Get-Installer()
             "Name" = "Firefox"
             "Uri" = "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win64&lang=en-US&_gl=1"
             "Install" = { Start-Process -Wait $Installer /quiet }
+        },
+        @{
+            "Name" = "Inkscape"
+            "Uri" = {
+                $(Get-RedirectedUrl https://inkscape.org/en/release) -Match "inkscape-([0-9]+\.[0-9]+(?:\.[0-9]+))" | Out-Null
+                $Version = $Matches[1]
+                $Msi = Invoke-Webrequest `
+                    -UseBasicParsing `
+                    "https://inkscape.org/release/inkscape-${Version}/windows/64-bit/msi/dl/" `
+                        | Select-Object -ExpandProperty Links `
+                        | Where-Object { $_ -Match "(inkscape-[0-9]+\.[0-9]+(?:\.[0-9]+).*\.msi)"} `
+                        | Select-Object -First 1 -ExpandProperty href
+
+                return "https://inkscape.org/${Msi}"
+            }
+            "Install" = { Start-Process -Wait $Installer /quiet }
         }
     )
+
+# --- https://www.powershellgallery.com/packages/Wormies-AU-Helpers/0.4.1 ---
+
+<#
+.SYNOPSIS
+    Aquire the url that is being redirected to when using the passed url parameter
+
+.DESCRIPTION
+    When a website only supplies a url to the latest version, but that url is redirected
+    to a different url to aquire the actual binary file.
+    Then this function can be used to simplify that aquiral.
+
+.PARAMETER url
+    The url to check for redirection
+
+.PARAMETER referer
+    An optional parameter to use when a website requires the referer header to
+    be used
+
+.OUTPUTS
+    The redirected url when one is found, otherwise returns the same url that was passed.
+
+.LINK
+    https://wormiecorp.github.io/Wormies-AU-Helpers/docs/functions/get-redirectedurl
+#>
+function Get-RedirectedUrl {
+    param(
+        [Parameter(Mandatory = $true)]
+        [uri]$url,
+        [uri]$referer,
+        [Alias('DisableEscape','RawUrl')]
+        [switch]$NoEscape
+    )
+
+    $req = [System.Net.WebRequest]::CreateDefault($url)
+    if ($referer) {
+        $req.Referer = $referer
+    }
+    $resp = $req.GetResponse()
+
+    if ($resp -and $resp.ResponseUri.OriginalString -ne $url) {
+        Write-Verbose "Found redirected url '$($resp.ResponseUri)"
+        if ($NoEscape -or $($resp.ResponseUri.OriginalString) -match '\%\d+' ) {
+            $result = $resp.ResponseUri.OriginalString
+        }
+        else {
+            $result = [uri]::EscapeUriString($resp.ResponseUri.OriginalString)
+        }
+    }
+    else {
+        Write-Warning "No redirected url was found, returning given url."
+        $result = $url
+    }
+
+    $resp.Dispose()
+
+    return $result
+}
+
+# ---
 
     function Test-IsAdministrator
     {
